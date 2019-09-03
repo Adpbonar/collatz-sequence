791ebdf00e88669dd8c6c4a64b21672b4cbce145
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2019-09-03 23:22:00 +0000

    Submission

---
+++ collatz.rb
@@ -0,0 +1,34 @@
+
+def collatz(n)
+  sequence = []
+  until n == 1
+    if n%2 != 0 
+        n = 3 * n + 1
+        sequence.push(n)
+      else
+        n = n/2
+        sequence.push(n)
+    end
+  end
+  return sequence
+end
+
+def iterator
+  counter = 1
+  current_longest_sequence = []
+  until counter == 1000000
+    seq = collatz(counter)
+    if seq.length > current_longest_sequence.length
+      current_longest_sequence = seq
+      counter+=1
+    else
+      counter+=1
+    end
+  end
+    puts "Starting number is #{current_longest_sequence[0]}.  
+    Sequence length is #{current_longest_sequence.length}"
+end
+
+collatz(1)
+
+iterator
